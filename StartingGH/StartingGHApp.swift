1e50f71b4bf8f917e15d178cbc179da84faa7411
samtechy26 <samkan061@gmail.com>
2022-03-08 15:41:15 -0500

    Initial Commit

---
+++ StartingGH/StartingGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  StartingGHApp.swift
+//  StartingGH
+//
+//  Created by Sam Tech on 3/8/22.
+//
+
+import SwiftUI
+
+@main
+struct StartingGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
